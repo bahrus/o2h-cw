c7c0d48e64f5fe2a8abfd642792f0793769396dd
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-26 17:44:10 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {o2h} from '../node_modules/o2h/o2h';
+
+export function doUsage(sw: (s: string) => void) : Promise<void>{
+  return new Promise((resolve, reject) => {
+    const usage = {
+      proxyURL: 'https://example.com/proxy',
+    }
+    const instance = o2h(usage, sw);
+    instance.addEventListener('done', e => {
+      resolve();
+    })
+  });
+}
 
 export default {
   async fetch(request: Request): Promise<Response> {
-    return new Response("Hello World!", {
+    const arr: string[] = [];
+
+    const sw = function(s: string){
+      arr.push(s);
+    }
+
+    let responseStr = "Hello World!"
+    if(!request.headers.has('proxy-url')){
+      await doUsage(sw);
+      responseStr = arr.join('\n');
+    }
+
+    return new Response(responseStr, {
       headers: {
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Allow-Origin': '*',
+        'content-type': 'text/html;charset=UTF-8',
       }
     });
   },
