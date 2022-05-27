2be5415eca1de7a99a93e4fc2eb981298d8d0001
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-27 10:10:17 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import {o2h} from '../node_modules/o2h/o2h';
+import {html} from '../node_modules/may-it-be/html';
+import json from './usageConfig.json' assert {type: 'json'};
 
 export function doUsage(sw: (s: string) => void) : Promise<void>{
-  return new Promise((resolve, reject) => {
+  console.log(json);
+  return new Promise((resolve) => {
     const usage = {
       proxyURL: 'https://example.com/proxy',
-    }
-    const instance = o2h(usage, sw);
+    };
+    const instance = o2h(usage, sw, json);
     instance.addEventListener('done', e => {
       resolve();
-    })
+    });
   });
 }
 
@@ -22,8 +25,26 @@ export default {
 
     let responseStr = "Hello World!"
     if(!request.headers.has('proxy-url')){
+
       await doUsage(sw);
-      responseStr = arr.join('\n');
+      responseStr = html`
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+          @import "https://unpkg.com/open-props@1.3.16";
+          @import "https://unpkg.com/open-props@1.3.16/normalize.min.css";
+          @import "https://unpkg.com/open-props/buttons.min.css";
+    </style>
+  </head>
+  <body>
+    ${arr.join('\n')}
+  </body>
+</html>
+         
+      `;  
     }
 
     return new Response(responseStr, {
